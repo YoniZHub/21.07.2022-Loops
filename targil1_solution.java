238b4418c953492cc4569409387bd0533766924b
YoniZHub <109853620+YoniZHub@users.noreply.github.com>
2022-07-23 15:27:38 +0300

    seif4 & seif5 added
    seif4 & seif5 solved and tested

--- targil1_solution.java
+++ targil1_solution.java
@@ -4,7 +4,9 @@ public class targil1_solution {
     public static void main(String[] args) {
         //seif1();
         //seif2();
-        seif3();
+        //seif3();
+        //seif4();
+        seif5();
     }
 
     public static void seif1() {
@@ -52,7 +54,35 @@ public class targil1_solution {
 
     }
 
+    public static void seif4(){
 
+       //while solution
+        int x = 4;
+        while (x < 101){System.out.println(x);
+            x = x + 2;
+        }
+
+        //for solution
+        for (int i =4; i < 101; i = i + 2){
+            System.out.println(i);
+        }
 
     }
+
+    public static void seif5(){
+        int a,b;
+        do {
+            Scanner numInput = new Scanner(System.in);
+            System.out.println("please enter int a");
+            a = numInput.nextInt();
+            System.out.println("please enter int b");
+            b = numInput.nextInt();
+
+            System.out.println("enter equals numbers");
+        }
+        // while (a != b;) {
+        while (!(a == b)) ;
+
+        }
+}
 
